ed716733d1cdc6ae0ae5dace883ef5543224965f
marziarezayee11 <mandgarmarziaroya@gmail.com>
2026-09-14 22:04:48 +0430

    first commit

---
+++ android/app/src/main/kotlin/com/example/cecondapp/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.cecondapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false, 
+      home: Scaffold(
+        
+        // ۱. نوار بالای صفحه همراه با آیکون سرچ
+        appBar: AppBar(
+          backgroundColor: Colors.orange,
+          title: Text('My App'),
+          actions: [
+            Icon(Icons.search), // آیکون جستجو
+            SizedBox(width: 15), // فاصله از راست
+          ],
+        ),
+        
+        // ۲. بدنه اصلی صفحه
+        body: Padding(
+          padding: EdgeInsets.all(20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              
+              // بخش نام همراه با آیکون آدمک (پروفایل)
+              Row(
+                children: [
+                  Icon(Icons.account_circle, size: 45, color: Colors.orange), // آیکون پروفایل
+                  SizedBox(width: 10), // فاصله آیکون تا متن
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('Layla Ahmadi', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+                      Text('Flutter developer', style: TextStyle(color: Colors.grey)),
+                    ],
+                  ),
+                ],
+              ),
+              
+              SizedBox(height: 30), 
+              
+              // دکمه‌های شهرها همراه با آیکون لوکیشن
+              Row(
+                children: [
+                  
+                  // دکمه کابل با آیکون
+                  ElevatedButton.icon(
+                    onPressed: () {}, 
+                    icon: Icon(Icons.location_on, size: 16), // آیکون لوکیشن
+                    label: Text('Kabul'), // متن دکمه
+                  ),
+                  SizedBox(width: 8), 
+                  
+                  // دکمه غزنی با آیکون
+                  ElevatedButton.icon(
+                    onPressed: () {}, 
+                    icon: Icon(Icons.location_on, size: 16),
+                    label: Text('Ghazni'),
+                  ),
+                  SizedBox(width: 8),
+                  
+                  // دکمه هرات با آیکون
+                  ElevatedButton.icon(
+                    onPressed: () {}, 
+                    icon: Icon(Icons.location_on, size: 16),
+                    label: Text('Herat'),
+                  ),
+                  
+                ],
+              ),
+
+            ],
+          ),
+        ),
+      ),
+    ),
+  );
+}
